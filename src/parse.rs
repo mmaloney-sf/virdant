f73fad72ff8d9c2e4e8a0ad1f1e402273bfa62df
Michael Maloney <michael@maloney.at>
2024-05-05 20:24:35 -0700

    WIP

--- src/parse.rs
+++ src/parse.rs
@@ -23,3 +23,91 @@ pub fn parse_package(package_text: &str) -> Result<Package, VirdantError> {
         Err(err) => Err(VirdantError::ParseError(format!("{err:?}"))),
     }
 }
+
+mod lr {
+    #![allow(dead_code)]
+
+    #[derive(PartialEq, Eq, Clone, Debug, Hash)]
+    enum Token {
+        C,
+        D,
+    }
+
+    #[derive(PartialEq, Eq, Clone, Debug, Copy, Hash)]
+    enum Nt {
+        Start,
+        S,
+        C,
+    }
+
+    enum Slot {
+        Token(Token),
+        Nt(Nt),
+        End,
+    }
+
+    type ProdId = usize;
+
+    struct Prod(Nt, Vec<Slot>);
+
+    struct Grammar {
+        prods: Vec<Prod>,
+    }
+
+    struct Item {
+        prod: Prod,
+        dot: usize,
+    }
+
+    use std::collections::HashSet;
+
+    impl Grammar {
+        fn production(&self, production_id: ProdId) -> &Prod {
+            &self.prods[production_id]
+        }
+
+        fn productions_by_nt(&self, nt: Nt) -> Vec<&Prod> {
+            let mut result = vec![];
+            for production@Prod(lhs, rhs) in &self.prods {
+                if *lhs == nt {
+                    result.push(production);
+                }
+            }
+            result
+        }
+
+        fn first(&self, nt: Nt) -> HashSet<Token> {
+            let mut result = HashSet::new();
+            for Prod(lhs, rhs) in &self.prods {
+                if *lhs == nt {
+                    let first_slot = rhs.first().unwrap();
+                    match first_slot {
+                        Slot::Token(c) => {
+                            result.insert(c.clone());
+                        },
+                        Slot::Nt(nt2) => {
+                            result.extend(self.first(*nt2).iter().cloned());
+                        },
+                        Slot::End => (),
+                    }
+                }
+            }
+            result
+        }
+    }
+
+    #[test]
+    fn lr_test() {
+        let grammar = Grammar {
+            prods: vec![
+                Prod(Nt::Start, vec![Slot::Nt(Nt::S), Slot::End]),
+                Prod(Nt::S, vec![Slot::Nt(Nt::C), Slot::Nt(Nt::C)]),
+                Prod(Nt::C, vec![Slot::Token(Token::C), Slot::Nt(Nt::C)]),
+                Prod(Nt::C, vec![Slot::Token(Token::D)]),
+            ],
+        };
+
+        dbg!(grammar.first(Nt::Start));
+        assert!(false);
+    }
+}

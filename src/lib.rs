e9180fd15fa4dd75a425be79c7b9eb0c750eeb9f
Michael Maloney <michael@maloney.at>
2024-07-15 18:03:37 +0000

    Remove old db.

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,11 @@ pub mod parse;
 pub mod context;
 pub mod value;
 pub mod types;
-pub mod db;
 pub mod common;
-pub mod elab;
-pub mod sim;
 // pub mod vcd;
 // pub mod mlir;
 pub mod verilog;
+
+//pub mod db;
+//pub mod elab;
+//pub mod sim;

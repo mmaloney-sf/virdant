3809db01d3770164e119c7c092100383d2ad6f84
Michael Maloney <michael@maloney.at>
2024-07-26 10:16:07 -0700

    Add part of a pest grammar for Virdant.

--- src/parse.rs
+++ src/parse.rs
@@ -6,6 +6,36 @@ use lalrpop_util::lexer::Token;
 use crate::ast::{Package, AstGen, Ast};
 use crate::common::*;
 
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct PackageParser;
+
+#[test]
+fn parse() {
+    use pest::Parser;
+    let example = "
+
+import foo;
+
+mod Foo {
+    incoming clock : Clock;
+    outgoing out : Word[8];
+    port mem of Mem;
+
+    reg counter : Word[8] on clock;
+    modfoo of Foo;
+
+    foo.inp := counter;
+}
+";
+    let parse = dbg!(PackageParser::parse(Rule::package, example)).unwrap();
+    for token in parse.tokens() {
+        eprintln!("{token:?}");
+    }
+}
+
 pub fn parse_package(package_name: &str, package_text: &str) -> VirdantResult<Ast<Package>> {
     let mut gen = AstGen::new(package_name);
     let result: Result<Ast<Package>, ParseError<usize, Token<'_>, &'static str>>

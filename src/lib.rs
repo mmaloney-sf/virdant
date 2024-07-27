d8d8bf18bc8709c10f674111e68baf3f61c6afa9
Michael Maloney <michael@maloney.at>
2024-07-26 20:41:07 -0700

    Add doc comments to parse.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+/// Utilities for parsing a Virdant source file.
+///
+/// [`parse_package()`](parse::parse_package) is used to parse a package.
+/// This results in a [`ParseTree`](parse::ParseTree) object (or a [`ParseError`](parse::ParseError) on failure).
 pub mod parse;
 
 #[cfg(test)]
--- src/parse.rs
+++ src/parse.rs
@@ -8,38 +8,43 @@ use pest_derive::Parser;
 #[grammar = "grammar.pest"]
 struct Parser;
 
+/// A node of the parse tree
 #[derive(Debug, Clone)]
 pub struct ParseTree<'a>(Pair<'a, Rule>);
 
+/// Parse a Virdant package
 pub fn parse_package(text: &str) -> Result<ParseTree, ParseError> {
     use pest::Parser as PestParser;
-    let r = Parser::parse(Rule::package, text)
+    Parser::parse(Rule::package, text)
         .map(|mut pairs| ParseTree(pairs.next().unwrap()))
-        .map_err(|err| ParseError(err));
-
-    dbg!(&r);
-    r
+        .map_err(|err| ParseError(err))
 }
 
+/// A line-col pair (1-indexed)
 #[derive(Debug, Clone, Copy)]
 pub struct Pos(usize, usize);
 
+/// A start-end position pair
 #[derive(Debug, Clone, Copy)]
 pub struct Span(Pos, Pos);
 
 impl<'a> ParseTree<'a> {
+    /// What rule produced this node in the parse tree?
     pub fn rule(&self) -> Rule {
         self.pair().as_rule()
     }
 
+    /// Get a child node with a given tag.
     pub fn get(&'a self, tag: &'a str) -> Option<ParseTree<'a>> {
         self.pair().clone().into_inner().find_first_tagged(tag).map(|pair| ParseTree(pair))
     }
 
+    /// Get the underlying string for a child node with a given tag.
     pub fn get_as_str(&'a self, tag: &'a str) -> Option<&'a str> {
         self.pair().clone().into_inner().find_first_tagged(tag).map(|pair| pair.as_str())
     }
 
+    /// Get the span in the source file for this node of the parse tree.
     pub fn span(&self) -> Span {
         let span = self.pair().as_span();
         let (start_line, start_col) = span.start_pos().line_col();
@@ -47,6 +52,7 @@ impl<'a> ParseTree<'a> {
         Span(Pos(start_line, start_col), Pos(end_line, end_col))
     }
 
+    /// Get the child nodes of this node in the parse tree.
     pub fn children(&self) -> impl Iterator<Item = ParseTree> {
         let inner = self.pair().clone().into_inner();
         inner
@@ -63,6 +69,7 @@ impl<'a> ParseTree<'a> {
             .unwrap()
     }
 
+    /// Get the underlying string for this node of the parse tree.
     pub fn as_str(&self) -> &str {
         self.pair().as_str()
     }
@@ -73,29 +80,35 @@ impl<'a> ParseTree<'a> {
 }
 
 impl Pos {
+    /// The line numbrer (1-indexed).
     pub fn line(&self) -> usize {
         self.0
     }
 
+    /// The column numbrer (1-indexed).
     pub fn col(&self) -> usize {
         self.1
     }
 }
 
 impl Span {
+    /// The start position.
     pub fn start(&self) -> Pos {
         self.0
     }
 
+    /// The end position.
     pub fn end(&self) -> Pos {
         self.1
     }
 }
 
+/// An error encountered during parsing.
 #[derive(Debug, Clone)]
 pub struct ParseError(Error<Rule>);
 
 impl ParseError {
+    /// Where in the source file did the span occur.
     pub fn span(&self) -> Span {
         match self.err().line_col {
             LineColLocation::Pos((line, col)) => {
